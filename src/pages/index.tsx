ec1a2591a9743ac7683feacf64178af0c911d96c
Maycon Reis Rosário <mayconrr13@gmail.com>
2021-06-02 11:05:02 -0300

    add initial settings

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Home | ig.news</title>
+    </Head>
+    <h1>Hello NextJS</h1>
+    </>
+  )
+}
