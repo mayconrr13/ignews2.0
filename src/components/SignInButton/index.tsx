01b5b4976a5ea2989c957f795c866701d0ca4f20
Maycon Reis Rosário <mayconrr13@gmail.com>
2021-06-02 19:14:16 -0300

    add nextauth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,16 +1,22 @@
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
 import styles from './SignInButton.module.scss'
 
 export const SignInButton = () => {
-  const isSignedIn = false
+  const [session] = useSession()
+  // console.log(session)
 
   return (
-    <button className={styles.signInButton} >
-      <FaGithub color={isSignedIn ? '#04d361' : '#eba417'}/>
-      {isSignedIn ? 'Maycon' : 'Sign in with Github' }
-      {isSignedIn && <FiX /> }
+    <button 
+      className={styles.signInButton} 
+      onClick={() => session ? signOut() : signIn('github')} 
+    >
+      <FaGithub color={session ? '#04d361' : '#eba417'}/>
+      {session ? session.user.name : 'Sign in with Github' }
+      {session && <FiX /> }
     </button>
   )
 }
